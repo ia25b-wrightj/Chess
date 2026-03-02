15465ba75b2e767d96acb9ae336eea1105d83982
Rami <hammoudir@bzz.ch>
2026-03-02 11:22:23 +0100

    asdf

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ const board = document.getElementById("board");
 function createBoard() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            const square = document.createElement("td");
+            const square = document.createElement("div");
             square.classList.add("square");
             if ((i + j) % 2 === 0) {
                 square.classList.add("light");
